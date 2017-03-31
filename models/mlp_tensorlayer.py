941c57731d92c3cf5ba212d059f3cb1175acddb9
Ubuntu <ubuntu@ip-172-31-13-31.ec2.internal>
2017-03-31 07:31:13 +0000

    Update in gigits, still not complete

--- models/mlp_tensorlayer.py
+++ models/mlp_tensorlayer.py
@@ -5,7 +5,7 @@ import tensorlayer as tl
 class Model:
     """ This is fully-connected net with several hidden layers. """
     
-    def __init__(self, neurons, losses, activation = 'relu', optimizer = 'Adam', biases = False):
+    def __init__(self, neurons, losses, activation = 'relu', optimizer = 'Adam', biases = False, mask = False):
         
         tl.layers.set_name_reuse(True)
         self.graph = tf.Graph()
@@ -16,7 +16,9 @@ class Model:
 
             # define input placeholder and layer
             self.x = tf.placeholder(tf.float32, shape=[None, neurons[0]], name='x')
-            self.y_ = tf.placeholder(tf.int64, shape=[None, ], name='y_') 
+            self.y_ = tf.placeholder(tf.int64, name='y_') 
+            self.s = tf.placeholder(tf.float32, name='s') 
+            
             self.network = tl.layers.InputLayer(self.x, name='input_layer') 
 
             f = {}
@@ -36,40 +38,56 @@ class Model:
             # define hidden layers
             for index, h_units in enumerate(neurons[1:-1]):
                 self.network = tl.layers.DenseLayer(self.network, n_units=h_units,
-                                    act=activation, b_init=None, name='relu{0}'.format(index+1))
+                                    act=activation, b_init=bias_init, name='relu{0}'.format(index+1))
                 
                 # define w* and fisher coefs
-                w['relu{0}'.format(index+1)] = tf.Variable(tf.random_normal([neurons[index], h_units]))
-                f['relu{0}'.format(index+1)] = tf.Variable(tf.zeros([neurons[index], h_units]))
+                w['relu{0}/W'.format(index+1)] = tf.Variable(tf.random_normal([neurons[index], h_units]))
+                f['relu{0}/W'.format(index+1)] = tf.Variable(tf.zeros([neurons[index], h_units]))
+                
+                if biases == True:
+                        w['relu{0}/b'.format(index+1)] = tf.Variable(tf.zeros([h_units]))
+                        f['relu{0}/b'.format(index+1)] = tf.Variable(tf.zeros([h_units]))
 
                 
             # define output layer
             self.network = tl.layers.DenseLayer(self.network, n_units=neurons[l-1],
                                     act = tf.identity,
-                                    b_init=None,
+                                    b_init=bias_init,
                                     name='output_layer')
             
-            w['output_layer'] = tf.Variable(tf.random_normal([neurons[l-2], neurons[l-1]]))
-            f['output_layer'] = tf.Variable(tf.zeros([neurons[l-2], neurons[l-1]]))
+            w['output_layer/W'] = tf.Variable(tf.random_normal([neurons[l-2], neurons[l-1]]))
+            f['output_layer/W'] = tf.Variable(tf.zeros([neurons[l-2], neurons[l-1]]))
+            
+            if biases == True:
+                w['output_layer/b'.format(index+1)] = tf.Variable(tf.zeros([neurons[l-1]]))
+                f['output_layer/b'.format(index+1)] = tf.Variable(tf.zeros([neurons[l-1]]))
 
+            
             self.l1 = tf.constant(losses['l1'])
             self.l2 = tf.constant(losses['l2'])
             self.ewc = tf.constant(losses['ewc'])
             self.f = f
             self.w = w
             
-            # define cost function and metric
             y = self.network.outputs
-            self.cost = tl.cost.cross_entropy(y, self.y_, 'cost')
-
-            correct_prediction = tf.equal(tf.argmax(y, 1), self.y_)
+            
+            # define cost function and metric
+            if mask == False:
+                self.cost = tl.cost.cross_entropy(y, self.y_, 'cost')
+            if mask == True:
+                yn = tf.multiply(y,self.s)
+                self.cost = tl.cost.cross_entropy(yn, self.y_, 'cost')
+                
+            correct_prediction = tf.equal(tf.argmax(tf.multiply(y, self.s), 1), self.y_)
             self.acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
             self.y_op = tf.argmax(tf.nn.softmax(y), 1)
             
             ewc_cost = self.cost
+            
             # define ewc cost
             for m in self.network.all_params:
-                name = m.name.split("/")[0]
+                name = m.name.split(":")[0]
+                print(name, m.get_shape().as_list())
                 ewc_cost = ewc_cost +  self.ewc * tf.nn.l2_loss( 
                     tf.multiply(self.f[name], tf.subtract(m, self.w[name])))
                 
